ba120735f2767676e6234207d956efb07f9b3622
Bassem <SanPatBRS@gmail.com>
2023-03-02 06:58:37 +0000

    Support and add Ubuntu driver shared library to timetagger

--- timetagger4_user_guide_example/src/timetagger4_user_guide_example.cpp
+++ timetagger4_user_guide_example/src/timetagger4_user_guide_example.cpp
@@ -1,10 +1,23 @@
 // timetagger4_user_guide_example.cpp : Example application for the TimeTagger4
-#include "timetagger4_interface.h"
+#include "TimeTagger4_interface.h"
 #include "stdio.h"
+#include "xTDC4_interface.h"
+#if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
+#endif
 
 typedef unsigned int uint32;
+#if defined(_WIN32) || defined(_WIN64)
 typedef unsigned __int64 uint64;
+typedef unsigned int uint32;
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+#define crono_sleep(x) Sleep(x)
+#else
+#include <unistd.h>
+#define crono_sleep(x) usleep(1000 * x)
+#endif
 
 timetagger4_device * initialize_timetagger(int buffer_size, int board_id, int card_index) {
 	// prepare initialization
@@ -118,9 +131,9 @@ void print_hit(uint32 hit, double binsize) {
 	printf("Hit @Channel %c - Flags %d - Offset %u (raw) / %.1f ns\n", channel, flags, ts_offset, ts_offset_ns);
 }
 
-_int64 processPacket(_int64 group_abs_time_old, volatile crono_packet *p, int update_count, double binsize) {
+__int64 processPacket(__int64 group_abs_time_old, volatile crono_packet *p, int update_count, double binsize) {
 	// do something with the data, e.g. calculate current rate
-	_int64 group_abs_time = p->timestamp;
+	__int64 group_abs_time = p->timestamp;
 	// group timestamp increments at 2 GHz
 	double rate = (2e9 / ((double)(group_abs_time - group_abs_time_old) / (double)update_count));
 	printf("\r%.2f kHz", rate / 1000.0);
@@ -184,8 +197,8 @@ int main(int argc, char* argv[]) {
 	int packets_with_errors = 0;
 	bool last_read_no_data = false;
 
-	_int64 group_abs_time = 0;
-	_int64 group_abs_time_old = 0;
+	__int64 group_abs_time = 0;
+	__int64 group_abs_time_old = 0;
 	int update_count = 100;
 	double binsize = get_binsize(device);
 
@@ -193,10 +206,11 @@ int main(int argc, char* argv[]) {
 	// read 10000 packets
 	while (packet_count < 10000)
 	{
+
 		// get pointers to acquired packets
 		status = timetagger4_read(device, &read_config, &read_data);
 		if (status != CRONO_OK) {
-			Sleep(100);
+			crono_sleep(100);
 			printf(" - No data! -\n");
 		}
 		else
@@ -205,6 +219,7 @@ int main(int argc, char* argv[]) {
 			volatile crono_packet* p = read_data.first_packet;
 			while (p <= read_data.last_packet)
 			{
+
 				// printf is slow, so this demo only processes every 1000th packet
 				// your application would of course collect every packet
 				if (packet_count % update_count == 0) {
